2bf5b737291897f4eb65bf7b7db780f67992d196
Omnish Kumar <omnish22@gmail.com>
2021-07-12 15:19:22 +0530

    Register page modify in validation

--- SubmitForm/app.js
+++ SubmitForm/app.js
@@ -1,3 +1,15 @@
+/*
+1- we can use querySelector or any selector at any branch i.e. document, parent or at child elements
+2- use preventDefault to prevent enter value from vanishing.
+3- className is use to extract element class name
+
+*/
+
+
+
+
+
+
 
 const username = document.querySelector(".username");
 const email = document.querySelector(".email");
@@ -8,6 +20,9 @@ function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+
+
 
 function showError(element,msg){
 
@@ -24,38 +39,24 @@ function showSuccess(element){
 }
 
 
+function checkRequired(elements){
+  elements.forEach(function(element){
+  if(element.value===""){
+    let className = element.className
+    if (className==="password2"){
+      className = "password"
+    }
+    showError(element,`${className[0].toUpperCase()}${className.slice(1)} Required`);
+  }else{
+    showSuccess(element);
+  }
+  })
+}
+
 form.addEventListener('submit',function(e){
   e.preventDefault();   /* to prevent value from vanishing quickly */
-  if(username.value === ""){
-    showError(username,"Username Required")
-  }else{
-      showSuccess(username);
-  }
-
-  if(email.value === ""){
-    showError(email,"Email Required");
-  }else if(!validateEmail(email.value)){
-      showError(email,"Enter Valid Email")
-  } else {
-    showSuccess(email);
-  }
-
-  if(password.value === ""){
-    showError(password,"Password Required")
-  }else{
-      showSuccess(password);
-  }
 
 
-  if(password2.value===""){
-      showError(password2,"Invalid");
-  }else if(password2.value === password.value){
-    showSuccess(password2);
-
-  }else if(password2.value === password.value){
-    showError(password2,"Password didn't match");
-  }
-
-
+  checkRequired([username,email,password,password2])
 
 })
